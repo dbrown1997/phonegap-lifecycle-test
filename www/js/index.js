cd6af90d93f3eb521f03262877a483374769b095
dbrown1997 <brod2_15@uni.worc.ac.uk>
2016-10-13 09:45:20 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var resumed_count = 0;
 var launched_count = 0;
 
     function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
+        document.addEventListener("deviceready", onDeviceReady, false);	//event Listener - check if devise is ready.
 		console.log("device ready");
     }
 	
@@ -17,10 +17,10 @@ var launched_count = 0;
     // device APIs are available
     //
     function onDeviceReady() {
-		alert("device ready");
+		alert("device ready");	//devise ready
         
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
+		document.addEventListener("resume", onResume, false);	//resume application
+		document.addEventListener("pause", onPause, false);	//pause application
 		
 		launched_count++;
 		updateDisplay();
@@ -28,13 +28,13 @@ var launched_count = 0;
 
     // Handle the pause event
     //
-    function onPause() {
+    function onPause() {	//pause app on pause
 		alert("pause");
 		paused_count++;
 		updateDisplay();
     }
 	
-	function onResume() {
+	function onResume() {	//resume app on resume
 		alert("resume");
 		resumed_count++;
 		updateDisplay();
