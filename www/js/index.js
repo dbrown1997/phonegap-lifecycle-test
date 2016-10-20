791b628f68603669258f5b545c64da791150e52b
dbrown1997 <brod2_15@uni.worc.ac.uk>
2016-10-20 10:55:12 +0100

    string

--- www/js/index.js
+++ www/js/index.js
@@ -5,42 +5,8 @@ var launched_count = 0;
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);	<!--event Listener - check if devise is ready.-->
 		console.log("device ready");
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
-
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("Device Ready");	//devise ready
-        
-		document.addEventListener("resume", onResume, false);	<!--resume application-->
-		document.addEventListener("pause", onPause, false);	<!--pause application-->
 		
-		launched_count++;
-		updateDisplay();
-    }
-
-    // Handle the pause event
-    //
-    function onPause() {	//pause app on pause
-		alert("pause");
-		paused_count++;
-		updateDisplay();
-    }
-	
-	function onResume() {	//resume app on resume
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
-
-	var key = "pen";
+		var key = "pen";
 	var value = "blue";
 	
 	var key = "window";
@@ -79,6 +45,44 @@ var launched_count = 0;
 			obj.sugars + "<br>" +
 			obj.protein + "<br>" +
 			obj.salt;
+		
+		
+    }
+	
+	function updateDisplay() {
+		$("#launched").text("Application launched: " + launched_count);
+		$("#resumed").text("Application paused: " + paused_count);
+		$("#paused").text("Application resumed: " + resumed_count);
+	}
+
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+		alert("Device Ready");	//devise ready
+        
+		document.addEventListener("resume", onResume, false);	<!--resume application-->
+		document.addEventListener("pause", onPause, false);	<!--pause application-->
+		
+		launched_count++;
+		updateDisplay();
+    }
+
+    // Handle the pause event
+    //
+    function onPause() {	//pause app on pause
+		alert("pause");
+		paused_count++;
+		updateDisplay();
+    }
+	
+	function onResume() {	//resume app on resume
+		alert("resume");
+		resumed_count++;
+		updateDisplay();
+    }
+
+	
 
 
 
